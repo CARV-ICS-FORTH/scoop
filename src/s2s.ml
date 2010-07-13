ac5a983c5cb5c6ff76589632f959b9596a1ceb19
Foivos S. Zakkak <zakkak@ics.forth.gr>
2010-07-13 14:59:35 +0000

    More code added to make_tpc_func

--- src/s2s.ml
+++ src/s2s.ml
@@ -65,7 +65,7 @@ and arg =
 
 (* FIXME: in_file should refer to a file not a list*)
 (* create 2 lists (the ppc output file and the tasks list) and create a ref to the input file*)
-let spu_tasks, ppc_glist, in_file, func_id = ref [], ref [], ref Cil.dummyFile , ref 0;
+let spu_tasks, ppc_glist, in_file, func_id = ref [], ref [], ref Cil.dummyFile , ref 0
 
 (* find the function definition of variable "name" in file f *)
 exception Found_fundec of fundec
@@ -101,6 +101,31 @@ let find_tcomp (f: file) (name: string) : typ =
     raise Not_found
   with Found_type t -> t
 
+(* find the variable named <name> in file f *)
+exception Found_var of varinfo
+let find_var (f: file) (name: string) : varinfo =
+  let findit = function
+    | GVarDecl(vi, _) when vi.vname = name -> raise (Found_var vi)
+    | GVar(vi, _, _) when vi.vname = name -> raise (Found_var vi)
+    | _ -> ()
+  in
+  try
+    Cil.iterGlobals f findit;
+    raise Not_found
+  with Found_var v -> v
+
+(* find the enum named <name> in file f *)
+exception Found_enum of enuminfo
+let find_enum (f: file) (name: string) : enuminfo =
+  let findit = function
+    | GEnumTag(ei, _) when ei.ename = name -> raise (Found_enum ei)
+    | _ -> ()
+  in
+  try
+    Cil.iterGlobals f findit;
+    raise Not_found
+  with Found_enum ei -> ei
+
 (* make a tpc_ version of the function (for use on the ppc side) *)
 let make_tpc_func (f: fundec) : global = begin
   print_endline ("Creating tpc_function_" ^ f.svar.vname);
@@ -111,7 +136,7 @@ let make_tpc_func (f: fundec) : global = begin
   (* unsigned int total_bytes *)
   let total_bytes = Cil.makeLocalVar f_new "total_bytes" Cil.uintType in
   (* volatile queue_entry_t *remote_entry *)
-  let remote_entry = Cil.makeLocalVar f_new "remote_entry" (TPtr((find_type !in_file "queue_entry_t"), [Attr("volatile", [])])) in
+(*   let remote_entry = Cil.makeLocalVar f_new "remote_entry" (TPtr((find_type !in_file "queue_entry_t"), [Attr("volatile", [])])) in *)
   (* volatile queue_entry_t *avail_task *)
   let avail_task = Cil.makeLocalVar f_new "avail_task" (TPtr((find_type !in_file "queue_entry_t"), [Attr("volatile", [])])) in
   (* TODO: add the #ifdef
@@ -124,6 +149,8 @@ let make_tpc_func (f: fundec) : global = begin
   let task_id_qs = Cil.makeLocalVar f_new "task_id_qs" (TPtr(Cil.uintType, [])) in
   (* unsigned int task_id *)
   let task_id = Cil.makeLocalVar f_new "task_id" Cil.uintType in
+  (* volatile completions_status_t *st *)
+  let st = Cil.makeLocalVar f_new "st" (TPtr((find_tcomp !in_file "completions_status_t"), [Attr("volatile", [])])) in
   (* if (f_new.sformals <> []) then begin *)
     (* void *arg_addr64 *)
     let arg_addr64 = Cil.makeLocalVar f_new "arg_addr64" voidPtrType in
@@ -142,23 +169,94 @@ let make_tpc_func (f: fundec) : global = begin
   (*** Initialize local variables ***)
   let stmts = ref [] in
   (* remote_entry=NULL *)
-  stmts := Cil.mkStmtOneInstr (Set (var remote_entry, CastE(voidPtrType ,zero), locUnknown))::!stmts;
+(*   stmts := Cil.mkStmtOneInstr (Set (var remote_entry, CastE(voidPtrType ,zero), locUnknown))::!stmts; *)
   (* avail_task=NULL *)
   stmts := Cil.mkStmtOneInstr (Set (var avail_task, CastE(voidPtrType ,zero), locUnknown))::!stmts;
-  (* arg_addr64=NULL *)
-  stmts := Cil.mkStmtOneInstr (Set (var arg_addr64, CastE(voidPtrType ,zero), locUnknown))::!stmts;
-  (* arg_size=0 *)
-  stmts := Cil.mkStmtOneInstr (Set (var arg_size, zero, locUnknown))::!stmts;
-  (* arg_flag=0 *)
-  stmts := Cil.mkStmtOneInstr (Set (var arg_flag, zero, locUnknown))::!stmts;
-  (* arg_stride=0 *)
-  stmts := Cil.mkStmtOneInstr (Set (var arg_stride, zero, locUnknown))::!stmts;
+  (* if (f_new.sformals <> []) then begin *)
+    (* arg_addr64=NULL *)
+    stmts := Cil.mkStmtOneInstr (Set (var arg_addr64, CastE(voidPtrType ,zero), locUnknown))::!stmts;
+    (* arg_size=0 *)
+    stmts := Cil.mkStmtOneInstr (Set (var arg_size, zero, locUnknown))::!stmts;
+    (* arg_flag=0 *)
+    stmts := Cil.mkStmtOneInstr (Set (var arg_flag, zero, locUnknown))::!stmts;
+    (* arg_stride=0 *)
+    stmts := Cil.mkStmtOneInstr (Set (var arg_stride, zero, locUnknown))::!stmts;
+  (* end *)
+  (* TODO: Add the #ifdef
+    #ifdef TPC_MULTITHREADED
+      pthread_mutex_lock( &tpc_callwait_mutex );
+    #endif
+    READ_TIME_REG(tmptime1);
+  *)
+  (* get the compl_status enuminfo *)
+  let compl_status_enum = find_enum !in_file "compl_status" in
+  (* get the s_available_spe varinfo *)
+  let g_max_spes = find_var !in_file "G_max_spes" in
+  (* get the s_available_spe varinfo *)
+  let s_available_spe = find_var !in_file "s_available_spe" in
+  (* get the task_queue_tail varinfo *)
+  let task_queue_tail = find_var !in_file "task_queue_tail" in
+  (* get the task_queue_tail varinfo *)
+  let compl_queue = find_var !in_file "compl_queue" in
+  (* get the g_task_current_id varinfo *)
+  let g_task_current_id = find_var !in_file "g_task_current_id" in
+  (* get the g_task_current_id varinfo *)
+  let g_task_id_queue = find_var !in_file "g_task_id_queue" in
+  (* get the task_queue varinfo *)
+  let task_queue = find_var !in_file "task_queue" in
+  (* create the while body *)
+  let w_body = ref [] in
+  (* create the [s_available_spe][task_queue_tail[s_available_spe]] offset *)
+  let big_offset = Index(Lval(var s_available_spe), Index(Lval((Var(task_queue_tail) , Index(Lval(var s_available_spe), NoOffset))), NoOffset)) in
+  (* st = &compl_queue[s_available_spe][task_queue_tail[s_available_spe]] *)
+  w_body := mkStmtOneInstr (Set (var st, AddrOf((Var compl_queue, big_offset)) , locUnknown))::!w_body;
+  let st_status = Lockutil.mkPtrFieldAccess (var st) "status" in
+  (* st->status = WAITING; break; *)
+  let bthen = mkBlock [ mkStmtOneInstr (Set (st_status, Const(CEnum(zero, "WAITING", compl_status_enum)), locUnknown) ); mkStmt (Break locUnknown) ] in
+  (* s_available_spe = (s_available_spe+1) % G_max_spes; *)
+  let next_spe_s = mkStmtOneInstr(Set (var s_available_spe, BinOp(Mod, BinOp(PlusA, Lval(var s_available_spe), one, intType), Lval(var g_max_spes), intType), locUnknown) ) in
+  let belse = mkBlock [ next_spe_s ] in
+  (*
+    if(st->status == COMPLETED) {
+      st->status = WAITING;
+      break;
+    } else {
+      s_available_spe = (s_available_spe+1) % G_max_spes;
+    }
+  *)
+  w_body := mkStmt(If(BinOp(Eq, Lval(st_status), Const(CEnum(one, "COMPLETED", compl_status_enum)), TInt(IBool, [])), bthen, belse, locUnknown))::!w_body;
+  (* push while in stmts *)
+  stmts := mkStmt(Loop( mkBlock (List.rev !w_body), locUnknown, None, None))::!stmts;
+  (* g_task_current_id[s_available_spe] *)
+  let gtc_indexed = (Var g_task_current_id, Index(Lval(var s_available_spe), NoOffset)) in
+  (** task_id = g_task_current_id[s_available_spe]++; **)
+  (* task_id = g_task_current_id[s_available_spe]; *)
+  stmts := mkStmtOneInstr(Set (var task_id, Lval(gtc_indexed), locUnknown))::!stmts;
+  (* g_task_current_id[s_available_spe]++; *)
+  stmts := mkStmtOneInstr(Set (gtc_indexed, BinOp(PlusA, Lval(gtc_indexed), one, intType), locUnknown))::!stmts;
+  (* task_id_qs = &g_task_id_queue[s_available_spe][task_queue_tail[s_available_spe]]; *)
+  stmts := mkStmtOneInstr(Set (var task_id_qs, AddrOf((Var g_task_id_queue, big_offset)) , locUnknown))::!stmts;
+  (** task_id = (task_id & 0x0FFFFFFF) | (s_available_spe << 28); **)
+  (* (task_id & 0x0FFFFFFF) *)
+  let lbs = BinOp(BAnd, Lval(var task_id), Const(CInt64(Int64.of_string "0x0FFFFFFF", IInt, None)), intType) in
+  (* (s_available_spe << 28) *)
+  let rbs = BinOp(Shiftlt, Lval(var s_available_spe), Const(CInt64(Int64.of_int 28, IInt, None)), intType) in
+  (* task_id = lbs | rbs; *)
+  stmts := mkStmtOneInstr(Set (var task_id, BinOp(BOr, lbs, rbs, intType), locUnknown))::!stmts;
+  (* *task_id_qs = task_id; *)
+  stmts := mkStmtOneInstr(Set ((mkMem (Lval(var task_id_qs)) NoOffset), Lval(var task_id), locUnknown))::!stmts;
+  (* TODO: READ_TIME_REG(tmptime2); *)
+  (* avail_task = &task_queue[s_available_spe][task_queue_tail[s_available_spe]]; *)
+  stmts := mkStmtOneInstr(Set (var avail_task, AddrOf((Var task_queue, big_offset)) , locUnknown))::!stmts;
 
   (* TODO: complete code *)
 
+  (* s_available_spe = (s_available_spe+1) % G_max_spes; *)
+  stmts := next_spe_s::!stmts;
   (* return task_id; *)
   stmts := Cil.mkStmt (Return (Some (Lval (var task_id)), locUnknown))::!stmts;
-  f_new.sbody <- mkBlock !stmts;
+  (* reverse the stmt list and put it in the body *)
+  f_new.sbody <- mkBlock (List.rev !stmts);
   GFun (f_new, locUnknown)
 end
 
